af0c955f9f6ea9874115e60b612125b23740f8c7
MSKnoGHB <242181889+MSKnoGHB@users.noreply.github.com>
2025-12-06 14:28:33 +0000

    [Finish]4-11

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,4 +1,5 @@
 class PostImagesController < ApplicationController
+  
   def new
     @post_image = PostImage.new
   end
@@ -11,6 +12,7 @@ class PostImagesController < ApplicationController
   end
 
   def index
+    @post_images = PostImage.all 
   end
 
   def show
