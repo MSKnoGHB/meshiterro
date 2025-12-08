55062665a97ea56cf22f0caeb64e3a7043141ef0
MSKnoGHB <242181889+MSKnoGHB@users.noreply.github.com>
2025-12-08 13:42:27 +0000

    [Wip]4-19

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -10,7 +10,7 @@ class PostCommentsController < ApplicationController
   private
 
   def post_comment_params
-    params.require(:post_comment).permit(:comment)
+    params.require(:post_commet).permit(:comment)
   end
 
 end
